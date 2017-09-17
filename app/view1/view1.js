ef4d816c578404bc2fb6284a33763dcf71621635
Emil <e.karamihov@student.fontys.nl>
2017-09-17 14:55:50 +0200

    Week 3

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,72 +1,43 @@
-
 'use strict';
 
 angular.module('myApp.view1', ['ngRoute'])
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.controller('View1Ctrl', [ '$scope', function($scope) {
-    $scope.departments = [
-            {   
-                "Name":"Management",
-                "Location":"NY.C"
-            },
-            {
-                "Name":"Sales",
-                "Location":"Chicago",
-            },
-            {
-                "Name":"Marketing",
-                "Location":"Boston"
-            },
-            {
-                "Name":"IT",
-                "Location":"Ohio"
-            }
+    .controller('View1Ctrl', [ '$scope', function($scope) {
+        console.log("in controller...");
+        $scope.newDepartment = {};
+        $scope.info = "";
+
+        $scope.departments = [
+            {name: "Marketing", location:"NYC"},
+            {name: "IT", location:"L.A."},
+            {name: "Sales", location:"Ohio"}
         ];
 
-    $scope.selectEdit = function (Id) {
-        var index = getSelectedIndex(Id);
-        var department = $scope.departments[index];
-        $scope.id = department.Id;
-        $scope.name = department.Name;
-        $scope.location = department.Location;
-    };
+        $scope.saveDepartment = function(){
+            console.log("Saving...");
+            $scope.departments.push($scope.newDepartment);
+            $scope.info = "New Department Added Successfully!";
+            $scope.newDepartment = {};
+        };
 
-    function getSelectedIndex(Id) {
-        for (var i=0; i<$scope.departments.length; i++)
-            if ($scope.departments[i].Id === Id)
-                return i;
-        return -1;
-    }
+        $scope.selectDepartment = function(department){
+            $scope.clickedDepartment = department;
+        };
 
-    $scope.add = function () {
-        $scope.departments.push({
-            "Id":$scope.Id ,
-            "Name":$scope.Name,
-            "Location":$scope.Location
-        });
-        $scope.Id = '';
-        $scope.Name = '';
-        $scope.Location = '';
-    };
+        $scope.deleteDepartment = function(){
+            console.log($scope.departments.indexOf($scope.clickedDepartment));
+            $scope.departments.splice($scope.departments.indexOf($scope.clickedDepartment), 1);
+            $scope.info = "Department Deleted Successfully!";
+        };
 
+        $scope.clearInfo = function(){
+            $scope.info = "";
+        };
 
-    $scope.edit = function () {
-        var index = getSelectedIndex(Id);
-        $scope.departments[index].Id = $scope.Id;
-        $scope.departments[index].Name = $scope.Name;
-        $scope.departments[index].Location = $scope.Location;
-    };
-
-    $scope.del = function (Id) {
-        var index = getSelectedIndex(Id);
-        alert(index);
-        $scope.departments.splice(index,1);
-    };
-
-}]);
+    }]);
