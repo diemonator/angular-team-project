cb5b847a7c496a30ba6f66b94f63ac84e66f29b0
Yoanna <yoanna.i.borisova@gmail.com>
2017-10-15 23:05:29 +0200

    Final commit

--- app/view4/view4.js
+++ app/view4/view4.js
@@ -9,28 +9,6 @@ angular.module('myApp.view4', ['ngRoute'])
         });
     }])
 
-    .directive('myDirective',function () {
-        return {
-            template: '<table class="table table-striped">\
-            <thead>\
-            <tr>\
-            <th>Employee</th>\
-            <th>Department</th>\
-            <th>Task</th>\
-            <th>Deadline</th>\
-            </tr>\
-            </thead>\
-            <tbody>\
-            <tr>\
-            <td>{{worker}} </td>\
-            <td>{{workPlace}}</td>\
-            <td>{{work}}</td>\
-            <td>{{endDate}}</td>\
-            </tr>\
-            </tbody>\
-            </table>'
-        }
-    })
     .service('WeatherService',['$http',function ($http) {
         this.getWeather = function () {
             return $http.get('https://api.openweathermap.org/data/2.5/weather?q=London&APPID=8ee783ade6c68579678b8e2ca073d450');
@@ -38,29 +16,35 @@ angular.module('myApp.view4', ['ngRoute'])
 
 
     }])
-    .factory('myFactory4',['myFactory1','myFactory2','myFactory3', function (myFactory1,myFactory2,myFactory3)
+    .factory('myFactory4',['Service' , function (Service)
     {
-        var id = 0;
-        var obj = {};
-        obj.data = [];
 
-        for (var i=0;i<myFactory3.data.length;i++)
-        {
-            obj.data.push({"id":i});
-            obj.data[i].departmentName = myFactory1.data[i].department;
-            obj.data[i].location = myFactory1.data[i].location;
-            //obj.data[i].employee = myFactory2.data[i].empName;
-            obj.data[i].task = myFactory3.data[i].task;
-            obj.data[i].time = myFactory3.data[i].deadline;
-        }
-        function ids() {
-            return id++;
-        }
-        return obj;
+            var obj = [];
+            Service.getAPIinfo('http://i874156.iris.fhict.nl/WEB2/departments')
+                .then(function (response) {
+                    obj.dep = response.data;
+                }, function (error) {
+                    $scope.error = error;
+                });
+
+            Service.getAPIinfo('http://i874156.iris.fhict.nl/WEB2/employees')
+                .then(function (response) {
+                    obj.emp = response.data;
+                }, function (error) {
+                    $scope.error = error;
+                });
+
+            Service.getAPIinfo('http://i874156.iris.fhict.nl/WEB2/tasks')
+                .then(function (response) {
+                    obj.tsk = response.data;
+                }, function (error) {
+                    $scope.error = error;
+                });
+
+            return obj;
     }])
 
-    .controller('View4Ctrl', [ '$scope','myFactory4','WeatherService', function($scope,myFactory4,WeatherService) {
-        $scope.infos = myFactory4.data;
+    .controller('View4Ctrl',['$scope','myFactory4','WeatherService','Service',function($scope,myFactory4,WeatherService, Service) {
 
         WeatherService.getWeather()
             .then(function (response) {
@@ -69,22 +53,171 @@ angular.module('myApp.view4', ['ngRoute'])
             },function (error) {
                 $scope.error = error;
             });
+        Service.getAPIinfo('http://i874156.iris.fhict.nl/WEB2/departments')
+            .then(function (response) {
+                $scope.department = response.data;
+
+            }, function (error) {
+                $scope.error = error;
+            });
+
+        Service.getAPIinfo('http://i874156.iris.fhict.nl/WEB2/employees')
+            .then(function (response) {
+                $scope.employee = response.data;
+            }, function (error) {
+                $scope.error = error;
+            });
+
+        Service.getAPIinfo('http://i874156.iris.fhict.nl/WEB2/tasks')
+            .then(function (response) {
+                $scope.task = response.data;
+            }, function (error) {
+                $scope.error = error;
+            });
+
+
 
         $scope.showTask = function (index) {
-            $scope.worker = $scope.infos[index].employee;
-            $scope.workPlace = $scope.infos[index].departmentName;
-            $scope.work = $scope.infos[index].task;
-            $scope.endDate = $scope.infos[index].time;
-    };
-        
-        $scope.showDep = function (index) {
-            $scope.worker = $scope.infos[index].employee;
-            $scope.workPlace = $scope.infos[index].location;
-            $scope.work = $scope.infos[index].departmentName;
-    };
-        
-        $scope.showEmp = function (index) {
-            $scope.worker = $scope.infos[index].employee;
-            $scope.workPlace = $scope.infos[index].departmentName;
-    };
-    }]);
+
+            Service.getAPIinfo('http://i874156.iris.fhict.nl/WEB2/tasks/'+index)
+                .then(function (response) {
+                    $scope.taskResult = response.data;
+                    console.log($scope.taskResult);
+                }, function (error) {
+                    $scope.error = error;
+                });
+
+        };
+
+        $scope.showDepartment = function (index) {
+
+            Service.getAPIinfo('http://i874156.iris.fhict.nl/WEB2/departments/'+index)
+                .then(function (response) {
+                    $scope.departmentResult = response.data;
+                    console.log($scope.departmentResult);
+                }, function (error) {
+                    $scope.error = error;
+                });
+
+        };
+
+        $scope.showEmployee = function (index) {
+
+            Service.getAPIinfo('http://i874156.iris.fhict.nl/WEB2/employees/'+index)
+                .then(function (response) {
+                    $scope.employeeResult = response.data;
+                    console.log($scope.employeeResult);
+                }, function (error) {
+                    $scope.error = error;
+                });
+
+        };
+
+
+    }])
+    .directive('myDirective',function () {
+        return {
+            template: '<table class="table table-striped">\
+            <thead>\
+            <tr>\
+            <th>No</th>\
+            <th>DeptNo</th>\
+            <th>Title</th>\
+            <th>Description</th>\
+            <th>Finished Date</th>\
+            <th>Modification Date</th>\
+            <th>creatioDate</th>\
+            <th>Employees</th>\
+            </tr>\
+            </thead>\
+            <tbody>\
+            <tr>\
+            <td>{{taskResult.no}} </td>\
+            <td>{{taskResult.deptNo}}</td>\
+            <td>{{taskResult.title}}</td>\
+            <td>{{taskResult.description}}</td>\
+            <td>{{taskResult.finishedDate}}</td>\
+            <td>{{taskResult.modificationDate}}</td>\
+            <td>{{taskResult.creatioDate}}</td>\
+            <td>\
+            <ul>\
+            <li ng-repeat="e in taskResult.employees">{{e.no}} date {{e.assignedDate}}</li>\
+            </ul>\
+            </td>\
+            </tr>\
+            </tbody>\
+            </table>'
+        }
+    })
+    .directive('myDirective1',function () {
+        return {
+            template: '<table class="table table-striped">\
+            <thead>\
+            <tr>\
+            <th>No</th>\
+            <th>Code</th>\
+            <th>Name</th>\
+            <th>Employees</th>\
+            <th>Tasks</th>\
+            </tr>\
+            </thead>\
+            <tbody>\
+            <tr>\
+            <td>{{departmentResult.no}} </td>\
+            <td>{{departmentResult.code}}</td>\
+            <td>{{departmentResult.name}}</td>\
+            <td>\
+            <ul>\
+            <li ng-repeat="emp in departmentResult.employees">{{emp.no}} from {{emp.fromDate}} to {{emp.toDate}}</li>\
+            </ul>\
+            </td>\
+            <td>{{departmentResult.tasks}}</td>\
+            </tr>\
+            </tbody>\
+            </table>'
+        }
+    })
+    .directive('myDirective2',function () {
+        return {
+            template: '<table class="table table-striped">\
+            <thead>\
+            <tr>\
+            <th>No</th>\
+            <th>BirthDate</th>\
+            <th>First Name</th>\
+            <th>Last Name</th>\
+            <th>Gender</th>\
+            <th>Hire Date</th>\
+            <th>Departments</th>\
+            <th>Titles</th>\
+            <th>Tasks</th>\
+            </tr>\
+            </thead>\
+            <tbody>\
+            <tr>\
+            <td>{{employeeResult.no}} </td>\
+            <td>{{employeeResult.birthDate}}</td>\
+            <td>{{employeeResult.firstName}}</td>\
+            <td>{{employeeResult.lastName}}</td>\
+            <td>{{employeeResult.gender}}</td>\
+            <td>{{employeeResult.hireDate}}</td>\
+            <td>\
+            <ul>\
+            <li ng repeat="dep in employeeResult.departments">{{dep.no}} from {{dep.fromDate}} to {{dep.toDate}}</li>\
+            </ul>\
+            </td>\
+            <td>\
+            <ul>\
+            <li ng repeat="title in employeeResult.titles">{{title.no}} from {{title.fromDate}} to {{title.toDate}} </li>\
+            </ul>\
+            </td>\
+            <td>\
+            <ul>\
+            <li ng repeat="tsk in employeeResult.tasks">{{tsk.no}} from {{title.assignedDate}} </li>\
+            </ul>\
+            </td>\
+            </tr>\
+            </tbody>\
+            </table>'
+        }
+    });
